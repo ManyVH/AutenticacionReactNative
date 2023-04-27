3bce6404a0aae54055e03e6e31224ccf016a80f3
José Manuel Varela <josemanuelvarela36@gmail.com>
2023-04-27 11:01:22 -0600

    Exposicion

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as LocalAuthentication from 'expo-local-authentication';
+//Para agregar la dependencia se usa ---> yarn expo install expo-local-authentication
+//https://docs.expo.dev/versions/latest/sdk/local-authentication/#localauthenticationoptions
+
+function autenticar() {
+  const auth = LocalAuthentication.authenticateAsync({
+    promptMessage:"Confirma tu identidad",
+    fallbackLabel:"Ingresa tu contraseña"
+  })
+  auth.then(resultado =>{
+    console.log(resultado)
+  })
+}
+async function tienesSensores() {
+    const soporte = await LocalAuthentication.hasHardwareAsync()
+    console.log(soporte)
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        onPress={ () => {
+          console.log(tienesSensores())
+          autenticar()
+        }}
+        color={"purple"}
+        title={"huella"}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
